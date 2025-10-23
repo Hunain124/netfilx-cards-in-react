23e18b48cc1d5e383e5457bc0c1a9ce044642f4b
hunain shahid <hunainshahid032@gmail.com>
2025-10-24 01:31:22 +0500

    Add styling on cards

--- learn-react/src/components/seriescards.jsx
+++ learn-react/src/components/seriescards.jsx
@@ -1,19 +1,20 @@
-export const Seriescards = ({data}) => {
-  const { id , img_url, name, rating , description, cast , genre , watch_url} = data;
-     return (
-          <li className="card-list">
-            <div>
-              <img src={img_url} className="card-images" alt="bads" />
-            </div>
-            <h2>Name: {name}</h2>
-            <h3>Rating: {rating}</h3>
-            <p className="summary-text">Summary: {description}</p>
-            <p>Cast: {cast}</p>
-            <p>Genre: {genre}</p>
-            <a href={watch_url} target="_blank">
-              <button className="btn-card">Watch Now</button>
-            </a>
-          </li>
-        );
+export const Seriescards = ({ data }) => {
+  const { id, img_url, name, rating, description, cast, genre, watch_url } = data;
+  const ratingClass = rating >= 8.5 ? "super_hit" : "average";
+  return (
+    <li className="card-list">
+      <div>
+        <img src={img_url} className="card-images" alt="bads" />
+      </div>
+      <h2>Name: {name}</h2>
+      <h3>Rating: <span className={`rating ${ratingClass}`} >{rating}</span></h3>
+      <p className="summary-text">Summary: {description}</p>
+      <p>Cast: {cast}</p>
+      <p>Genre: {genre}</p>
+      <a href={watch_url} target="_blank">
+        <button className={`btn-card ${ratingClass}`}>Watch Now</button>
+      </a>
+    </li>
+  );
 
 }
